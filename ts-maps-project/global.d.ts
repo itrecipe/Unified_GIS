11da4b824bea2355c63fb39efa44ce4fe6332235
IT Recipe <itrecipe@naver.com>
2024-12-19 16:15:39 +0900

    ts 프로젝트
    내일 다시 리펙토링

---
+++ ts-maps-project/global.d.ts
@@ -0,0 +1,7 @@
+export {};
+
+declare global {
+  interface Window {
+    kakao: any; // kakao 객체를 any로 선언
+  }
+}
--- ts-maps-project/src/index.tsx
+++ ts-maps-project/src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+// 'root'라는 ID를 가진 요소를 가져옵니다.
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element with id 'root' not found in index.html");
+}
+
+// ReactDOM.createRoot로 렌더링합니다.
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
