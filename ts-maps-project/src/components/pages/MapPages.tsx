6deafc1ee8da985f1fa7a2823fbf1c74772a3e8c
IT Recipe <itrecipe@naver.com>
2024-12-20 17:01:31 +0900

    카카오맵 프로젝트
    study

--- ts-maps-project/src/components/pages/MapPages.tsx
+++ ts-maps-project/src/components/pages/MapPages.tsx
@@ -3,26 +3,29 @@ import { Map, MapTypeControl, ZoomControl } from "react-kakao-maps-sdk"
 import useKakaoLoader from "../../hooks/useKaKaoLoader"
 
 export default function MapPages() {
-
   useKakaoLoader();
 
+  const mapRef = useRef<kakao.maps.Map>(null)
+
   return (
-    <Map // 지도를 표시할 Container
-      id="map"
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "350px",
-      }}
-      level={3} // 지도의 확대 레벨
+    <>
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={{
+          // 지도의 중심좌표
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          width: "100%",
+          height: "350px",
+        }}
+        level={3} // 지도의 확대 레벨
       >
-      <MapTypeControl position={"TOPRIGHT"} />
-      <ZoomControl position={"RIGHT"} />
-    </Map>
+        
+        <MapTypeControl position={"TOPRIGHT"} />
+        <ZoomControl position={"RIGHT"} />
+      </Map>
+    </>
   )
 }
