c6807493bc660e9438b69e6ec215373db8db54af
IT Recipe <itrecipe@naver.com>
2024-12-26 15:28:32 +0900

    코드 수정
    백업

--- ts-maps-project/src/hooks/useKaKaoLoader.tsx
+++ ts-maps-project/src/hooks/useKaKaoLoader.tsx
@@ -1,14 +1,34 @@
-import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
+import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
 
-export default function useKakaoLoader() {
+interface KakaoLoaderOptions {
+  appkey: string;
+  libraries: Array<"clusterer" | "drawing" | "services">;
+}
+
+export default function useKakaoLoader(): void {
   useKakaoLoaderOrigin({
-    /** 
+    /**
      * ※주의※ appkey의 경우 본인의 appkey를 사용하셔야 합니다.
      * 해당 키는 docs를 위해 발급된 키 이므로, 임의로 사용하셔서는 안됩니다.
-     * 
+     *
      * @참고 https://apis.map.kakao.com/web/guide/
      */
-    appkey: "f9a777a0a8a1aefe4e8e589283a46d28",
+    appkey: "자스키",
     libraries: ["clusterer", "drawing", "services"],
-  })
+  } as KakaoLoaderOptions);
 }
+
+// import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
+
+// export default function useKakaoLoader() {
+//   useKakaoLoaderOrigin({
+//     /** 
+//      * ※주의※ appkey의 경우 본인의 appkey를 사용하셔야 합니다.
+//      * 해당 키는 docs를 위해 발급된 키 이므로, 임의로 사용하셔서는 안됩니다.
+//      * 
+//      * @참고 https://apis.map.kakao.com/web/guide/
+//      */
+//     appkey: "f9a777a0a8a1aefe4e8e589283a46d28",
+//     libraries: ["clusterer", "drawing", "services"],
+//   })
+// }
